c60d0c7cf72a15f5e25ca7371e474835159938ba
jessy ocaña <jess.quimica.95@gmail.com>
2023-01-23 18:14:50 -0600

    reto completado

---
+++ encriptador.js
@@ -0,0 +1,54 @@
+
+
+
+const textoIngresado = document.querySelector("#texto");
+const textoResultado = document.querySelector("#texto-Resultado");
+
+const botonEncriptar = document.querySelector("#encriptar");
+const botonDesencriptar = document.querySelector("#desencriptar");
+const botonCopiar = document.querySelector("#copiar");
+
+function encriptar (){
+  let texto = textoIngresado.value.toLowerCase();
+  let textoEncriptado = texto
+  .replaceAll("e", "enter")
+  .replaceAll("i", "imes")
+  .replaceAll("o", "ober")
+  .replaceAll("a", "ai")
+  .replaceAll("u", "ufat");
+
+  document.getElementById("no-Resuelto").style.visibility = "hidden";
+  document.getElementById("copiar").style.visibility = "visible";
+
+  textoResultado.value = textoEncriptado;
+
+  document.getElementById("texto").value = '';
+
+}
+
+function desencriptar (){
+  let textoEncriptado = textoIngresado.value.toLowerCase();
+  let texto = textoEncriptado
+  .replaceAll("enter", "e")
+  .replaceAll("imes", "i")
+  .replaceAll("ober", "o")
+  .replaceAll("ai", "a")
+  .replaceAll("ufat", "u");
+
+  document.getElementById("no-Resuelto").style.visibility = "hidden";
+  document.getElementById("copiar").style.visibility = "visible";
+
+  textoResultado.value = texto;
+
+  document.getElementById("texto").value = '';
+
+}
+
+function copiar () {
+  let textoEncriptado = textoResultado.value;
+  navigator.clipboard.writeText(textoEncriptado);
+}
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+botonCopiar.onclick = copiar;
